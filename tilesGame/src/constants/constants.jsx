a92aa50e47ece538509d981a045a5cb6ca348ece
Alina Potapchuk <alinapotapchuk7@gmail.com>
2024-12-21 21:46:23 +0300

    fix of deploy

--- tilesGame/src/constants/constants.jsx
+++ tilesGame/src/constants/constants.jsx
@@ -1,12 +1,12 @@
 const cardImages = [
-  {'src': '../images/sea1.jpg'},
-  {'src': '../images/sea2.jpg'},
-  {'src': '../images/sea3.jpg'},
-  {'src': '../images/sea4.jpg'},
-  {'src': '../images/sea5.jpg'},
-  {'src': '../images/sea6.jpg'},
-  {'src': '../images/sea7.jpg'},
-  {'src': '../images/sea8.jpg'},
+  {'src': './images/sea1.jpg'},
+  {'src': './images/sea2.jpg'},
+  {'src': './images/sea3.jpg'},
+  {'src': './images/sea4.jpg'},
+  {'src': './images/sea5.jpg'},
+  {'src': './images/sea6.jpg'},
+  {'src': './images/sea7.jpg'},
+  {'src': './images/sea8.jpg'},
 ];
 
 export function randomizeCards() {
